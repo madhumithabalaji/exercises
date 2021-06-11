8de138260ac65a5a8d3b4f7603f9447e07eca2d5
Madhumitha Balaji <32056635+madhumithabalaji@users.noreply.github.com>
2021-06-10 20:46:04 -0400

    Create subStrNoRepeatChars.py

---
+++ subStrNoRepeatChars.py
@@ -0,0 +1,19 @@
+class Solution:    
+    def subCount(i, ip, count):
+        tempCount, tempStr = 1, []
+        while i >= -len(ip)+1:
+            tempStr.append(ip[i])
+            if ip[i] != ip[i-1] and ip[i-1] not in tempStr:
+                tempStr.append(ip[i-1])
+                tempCount+=1
+            elif ip[i] == ip[i-1]:
+                tempCount = 1
+                tempStr = []
+            i-=1
+            count.append(tempCount)
+            Solution.subCount(i, ip, count)
+        return max(count) if len(count) > 0 else 0
+    
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        count = []
+        return Solution.subCount(-1,s,count)
